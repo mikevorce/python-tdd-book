d3cff378519f79556ee95e4185f08831018acbe7
Mike Vorce <michael.j.vorce@gmail.com>
2019-05-19 15:04:27 -0500

    Refactor functional tests to be less repetitive
    This commit adds a 'check_for_row_in_todo_list' function to make
    the task of checking the contents of the rendered todo list in the
    home view of the project less repetitive.

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,13 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    # utility functions for tests -------------------------------------
+    def check_for_row_in_todo_list(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+    # test functions --------------------------------------------------
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Kirsten goes to check out the homepage of the to-do app
         self.browser.get('http://localhost:8000')
@@ -35,10 +42,7 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feather" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_todo_list('1: Buy peacock feathers')        
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly"
@@ -48,13 +52,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # The page updates again, and now shows both items on her list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn(
-                '2: Use peacock feathers to make a fly', 
-                [row.text for row in rows]
-        )
+        self.check_for_row_in_todo_list('1: Buy peacock feathers',)
+        self.check_for_row_in_todo_list('2: Use peacock feathers to make a fly')
 
         # Kirsten wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
